c93cda6faac0a4c7241334a7b3952d1fdf9c560c
UsuallyChris <schoonc@gmail.com>
2019-05-09 14:12:09 -0700

    initial commit

---
+++ blog/models.py
@@ -0,0 +1,18 @@
+""" Model definitions for blog app """
+
+from django.db import models
+
+# Create your models here.
+
+
+class Post(models.Model):
+    """ defines Post model in blog app """
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey(
+        'auth.User',
+        on_delete=models.CASCADE
+    )
+    body = models.TextField()
+
+    def __str__(self):
+        return self.title
