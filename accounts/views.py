f1a79ff28b6b3e4181cf128cfa5f698d298a094f
UsuallyChris <schoonc@gmail.com>
2019-05-10 16:32:41 -0700

    Fixed page rendering errors

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,4 @@
 """ Define views for accounts app """
-from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views import generic
@@ -10,4 +9,4 @@ class SignUpView(generic.CreateView):
     """ Define sign up view to create new users """
     form_class = UserCreationForm
     success_url = reverse_lazy('login')
-    template_name = 'signup.html'
+    template_name = 'registration/signup.html'
